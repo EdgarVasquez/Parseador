12e1943fed7da7a54494401b0aff9c58c70f1b0e
EdgarVasquez <EdgarVasquezpe@Gmail.com>
2022-11-16 15:36:41 -0400

    Agregar archivos de proyecto.

---
+++ Parseador/Program.cs
@@ -0,0 +1,13 @@
+﻿using Antlr4.Runtime;
+using Parseador;
+
+string input = "1+5*8+6";
+Console.WriteLine($"Input {input}\n"); 
+ICharStream stream = CharStreams.fromString(input);
+aritmeticaLexer lexer = new aritmeticaLexer(stream); 
+CommonTokenStream tokens = new CommonTokenStream(lexer); 
+aritmeticaParser parser = new aritmeticaParser(tokens);
+var tree = parser.aritmetica(); 
+Aritmetica aritmetica = new Aritmetica();
+//La clase que creamos
+int resultado= aritmetica.Visit(tree);
